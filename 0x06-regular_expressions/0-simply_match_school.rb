181e8887518872f40a8f90a7a436544a173caf64
DaiMika <maxdietunnie@gmail.com>
2024-03-12 06:05:19 -0700

    update

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+#we only want a particular word with case sensitive i only used the g
+#flag for global
+#by: maxyn edogha
+puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#by: maxyn edogha
+puts ARGV[0].scan(/hbt{2,5}n/).join
